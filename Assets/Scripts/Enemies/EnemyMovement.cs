07d20c451371ce8d92f074741e3694b920b1c58b
avz <alexgar0204@gmail.com>
2025-11-26 11:37:36 -0600

    ENEMIES SO
    Signed-off-by: avz <alexgar0204@gmail.com>

---
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
